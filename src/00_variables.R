1585813051b2bb5f0854e3b3b487123960bd8f31
jh22d <jh0220x@gmail.com>
2025-03-12 21:13:29 -0700

    pull macro statistics from world bank API

--- src/00_variables.R
+++ src/00_variables.R
@@ -1,2 +1,4 @@
 start_year=2011
 end_year=2018
+countries= c('CA','US')
+indicators_macro <- c('NY.GDP.PCAP.KD', 'FP.CPI.TOTL', 'FR.INR.RINR') # GDP,CPI,RIR
---
+++ src/02_import-macro-data.R
@@ -0,0 +1,13 @@
+library(WDI)
+library(tidyverse)
+
+# WDIsearch('real.*interest.*rate')
+macro <- WDI(indicator=indicators_macro,
+           country=countries,
+           start=start_year,
+           end=end_year) %>%
+  mutate(gdp=NY.GDP.PCAP.KD,
+         cpi=FP.CPI.TOTL,
+         rir=FR.INR.RINR,
+         country=factor(country))%>%
+  select(-iso2c,-iso3c,-indicators_macro)
