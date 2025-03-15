e1313ca128abe242f95ca1955365fea91cf2f2e6
jh22d <jh0220x@gmail.com>
2025-03-15 16:56:41 -0700

    mutate the time variable

--- src/04_data-clean.R
+++ src/04_data-clean.R
@@ -1,1 +1,10 @@
 # combine datasets in 01,02 and 03 files
+library(readr)
+library(tidyverse)
+library(lubridate)
+macro <- read_csv("data/processed/macro.csv")
+all_bank_bg <- read_csv("data/processed/all_bank_bg.csv")
+
+all_bank_df <- read_csv("data/processed/all_bank_df.csv")
+# convert the exact date to year and yearquarter 
+all_bank_df%>%mutate(year=year(date), quarter=quarter(date), t)
