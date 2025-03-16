4a2396049c8e9a3c1311790516dd9ae5a0900340
jh22d <jh0220x@gmail.com>
2025-03-15 17:36:40 -0700

    data prep for stata

--- src/00_variables.R
+++ src/00_variables.R
@@ -1,4 +1,5 @@
 start_year=2011
 end_year=2018
+policy_change_year=2014
 countries= c('CA','US')
 indicators_macro <- c('NY.GDP.PCAP.KD', 'FP.CPI.TOTL', 'FR.INR.RINR') # GDP,CPI,RIR
--- src/04_data-clean.R
+++ src/04_data-clean.R
@@ -6,5 +6,25 @@ macro <- read_csv("data/processed/macro.csv")
 all_bank_bg <- read_csv("data/processed/all_bank_bg.csv")
 
 all_bank_df <- read_csv("data/processed/all_bank_df.csv")
+
 # convert the exact date to year and yearquarter 
-all_bank_df%>%mutate(year=year(date), quarter=quarter(date), t)
+all_bank_time_fixed <- all_bank_df%>%
+  mutate(year=year(date), 
+         quarter=quarter(date), 
+         yr_qtr = paste0(year,"q",quarter))%>%
+  filter(year >= start_year & year <= end_year)%>%
+  select(-c(date,...1,  quarter))
+
+# combine 3 dataset
+bank_full <- all_bank_time_fixed%>%
+  left_join(all_bank_bg%>%select(-...1), by='bank')%>%
+  left_join(macro%>%select(-...1), by = c('year','country'))
+
+# convert indicator variables T, P and TP
+df_for_data <- bank_full %>%
+       mutate(T = ifelse(country == "Canada", 1, 0),
+              P = ifelse(year >= policy_change_year, 1, 0),
+              TP = T * P)
+
+
+
