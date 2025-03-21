6047774506b0c56050d4dbf51b67ec192741acfe
jh22d <jh0220x@gmail.com>
2025-03-21 14:25:24 -0700

    add loggdp and logcpi

--- src/02_macro.R
+++ src/02_macro.R
@@ -4,13 +4,25 @@ library(WDI)
 library(tidyverse)
 
 # WDIsearch('real.*interest.*rate')
-macro <- WDI(indicator=indicators_macro,
+macro_raw <- WDI(indicator=indicators_macro,
            country=countries,
            start=start_year,
-           end=end_year) %>%
+           end=end_year)
+macro <-  macro_raw %>%
   mutate(gdp=NY.GDP.PCAP.KD,
          cpi=FP.CPI.TOTL,
          rir=FR.INR.RINR,
-         country=factor(country))%>%
+         country=factor(country),
+         log_gdp = log(gdp),
+         log_cpi=log(cpi))%>%
   select(-iso2c,-iso3c,-indicators_macro)
 # write.csv(macro_df,"data/processed/macro.csv")
+
+# 
+# ggplot(macro, aes(x = year, y = log10_gdp, color = country)) +
+#   geom_line() +
+#   labs(title = "GDP Per Capita Over Time by Country",
+#        x = "Year",
+#        y = "GDP Per Capita (constant USD)",
+#        color = "Country") +
+#   theme_minimal()
